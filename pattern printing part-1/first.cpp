85cdf855f760431d7408c6b34826776bda5309cd
Sharath <sharathnalla026@gmail.com>
2026-02-23 18:36:24 +0530

    done

---
+++ pattern printing part-1/first.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+      
+//   1st method simple and easy
+
+// cout<<"* * * * *";
+
+// 2nd method 
+
+int n ;
+
+cout<<"Enter your Number=";
+cin>>n;
+
+for(int i =1 ; i<=n; i++){
+
+    cout<<"*";
+}
+
+
+
+return 0;
+
+}
---
+++ pattern printing part-1/nine.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    for(int i = 1;i<=5 ;i++){
+        for(int j =0 ;j<5 ;j++){
+         
+           char name  = 'a'+j;
+
+           cout<<name<<" ";
+
+        }
+
+        cout<<endl;
+    }
+
+
+
+    return 0;
+}
