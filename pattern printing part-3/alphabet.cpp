f2cfa199a84d81ffa47697b5c7a877549db35fff
Sharath <sharathnalla026@gmail.com>
2026-02-24 08:01:58 +0530

    done

---
+++ pattern printing part-3/alphabet.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    for(int i =1 ;i<=5; i++){
+        for(int j =1 ;j<=5-i ; j++){
+            cout<<"  ";
+            
+        }   
+        for(int j =1 ; j<=i ;j++){
+            char name = 'A'+(j-1);
+            
+            cout<<name<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+    return 0;
+}
